c77dbc7ed4df206640a6f9e7390cbe1b2ba533fb
LoicPi <lcpiriou@gmail.com>
2019-07-31 16:05:52 +0200

    Modification de la classe User et Role. Modification dans la génération de la clé primaire id.

--- src/main/java/com/adle/projet/entity/Role.java
+++ src/main/java/com/adle/projet/entity/Role.java
@@ -21,7 +21,7 @@ import javax.validation.constraints.Size;
 public class Role {
 
     @Id
-    @GeneratedValue( strategy = GenerationType.AUTO )
+    @GeneratedValue( strategy = GenerationType.IDENTITY )
     @Column( name = "id" )
     private Integer    id;
 
--- src/main/java/com/adle/projet/entity/User.java
+++ src/main/java/com/adle/projet/entity/User.java
@@ -34,7 +34,7 @@ import org.hibernate.validator.constraints.NotEmpty;
 public class User {
 
     @Id
-    @GeneratedValue( strategy = GenerationType.AUTO )
+    @GeneratedValue( strategy = GenerationType.IDENTITY )
     @Column( name = "id" )
     private Integer    id;
 
