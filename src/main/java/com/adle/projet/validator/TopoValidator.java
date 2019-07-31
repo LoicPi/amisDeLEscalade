28778db02de4b7f8dceca02504011314890d2c74
LoicPi <lcpiriou@gmail.com>
2019-07-31 16:13:05 +0200

    Creation de la classe TopoValidator. Afin de gérer les erreurs rentrées dans les différents champs de la page d'enregistrement des Topos, création de la classe validator de Topo. Modification du fichier messages.properties afin de mettre les messages d'erreurs.

---
+++ src/main/java/com/adle/projet/validator/TopoValidator.java
@@ -0,0 +1,56 @@
+package com.adle.projet.validator;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import com.adle.projet.entity.Topo;
+
+@Component
+public class TopoValidator implements Validator {
+
+    private static final Logger logger = LogManager.getLogger( UserLoggValidator.class );
+
+    @Override
+    public boolean supports( Class<?> clazz ) {
+        return Topo.class.equals( clazz );
+    }
+
+    @Override
+    public void validate( Object obj, Errors errors ) {
+        Topo topo = (Topo) obj;
+
+        if ( topo.getTopoName().equals( "" ) ) {
+            logger.info( "Topo_Name is empty" );
+            errors.rejectValue( "topoName", "topoValidator.topoName.empty" );
+        }
+
+        if ( topo.getTopoDescriptive().equals( "" ) ) {
+            logger.info( "Topo_Descriptive is empty" );
+            errors.rejectValue( "topoDescritpive", "topoValidator.topoDescriptive.empty" );
+        }
+
+        if ( topo.getTopoCity().equals( "" ) ) {
+            logger.info( "Topo_City is empty" );
+            errors.rejectValue( "topoCity", "topoValidator.topoCity.empty" );
+        }
+
+        if ( topo.getTopoCounty() == null ) {
+            logger.info( "Topo_County is empty" );
+            errors.rejectValue( "topoCounty", "topoValidator.topoCounty.empty" );
+        }
+
+        if ( topo.getTopoCountry().equals( "" ) ) {
+            logger.info( "Topo_Country is empty" );
+            errors.rejectValue( "topoCountry", "topoValidator.topoCountry.empty" );
+        }
+
+        if ( topo.getTopoReleaseDate().equals( "" ) ) {
+            logger.info( "Topo_ReleaseDate is empty" );
+            errors.rejectValue( "topoReleaseDate", "topoValidator.topoReleaseDate.empty" );
+        }
+    }
+
+}
