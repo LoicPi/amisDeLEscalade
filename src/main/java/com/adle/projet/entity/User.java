d5fc4c73d8e8bc07956dd876a82d4e378274c602
LoicPi <lcpiriou@gmail.com>
2019-08-06 10:53:33 +0200

    Modification sur la classe User. Modification sur la génération de l'id dans la table user.

--- src/main/java/com/adle/projet/entity/User.java
+++ src/main/java/com/adle/projet/entity/User.java
@@ -34,7 +34,7 @@ import org.hibernate.validator.constraints.NotEmpty;
 public class User {
 
     @Id
-    @GeneratedValue( strategy = GenerationType.IDENTITY )
+    @GeneratedValue( strategy = GenerationType.AUTO )
     @Column( name = "id" )
     private Integer    id;
 
