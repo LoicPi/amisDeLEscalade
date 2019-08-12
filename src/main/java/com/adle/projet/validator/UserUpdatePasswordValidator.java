c27fdf818c8418e332ef71548e6c8b8def5004f6
LoicPi <lcpiriou@gmail.com>
2019-08-12 17:33:22 +0200

    Modification sur la classe de validation du mot de passe. Nouveau contrôle de fait.

--- src/main/java/com/adle/projet/validator/UserUpdatePasswordValidator.java
+++ src/main/java/com/adle/projet/validator/UserUpdatePasswordValidator.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
-import com.adle.projet.entity.User;
+import com.adle.projet.dto.UpdateUser;
 
 @Component
 public class UserUpdatePasswordValidator implements Validator {
@@ -17,27 +17,39 @@ public class UserUpdatePasswordValidator implements Validator {
 
     @Override
     public boolean supports( Class<?> clazz ) {
-        return User.class.equals( clazz );
+        return UpdateUser.class.equals( clazz );
     }
 
     @Override
     public void validate( Object obj, Errors errors ) {
-        User user = (User) obj;
-
-        String userPassword = user.getPassword();
+        UpdateUser user = (UpdateUser) obj;
 
         Pattern patternPassword = Pattern.compile(
                 "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[-+!*$@%_])([-+!*$@%_\\w]{8,15})$", Pattern.CASE_INSENSITIVE );
 
-        if ( userPassword.equals( "" ) ) {
+        if ( user.getOldPassword().equals( "" ) ) {
             logger.info( "Password is empty." );
             errors.rejectValue( "password", "userLoggValidator.password.empty" );
         }
 
-        if ( !userPassword.equals( "" ) && !( patternPassword.matcher( user.getPassword() ).matches() ) ) {
+        if ( user.getNewPassword().equals( "" ) ) {
+            logger.info( "Password is empty." );
+            errors.rejectValue( "password", "userLoggValidator.password.empty" );
+        }
+
+        if ( user.getConfirmPassword().equals( "" ) ) {
+            logger.info( "Password is empty." );
+            errors.rejectValue( "password", "userLoggValidator.password.empty" );
+        }
+
+        if ( !user.getNewPassword().equals( "" ) && !( patternPassword.matcher( user.getNewPassword() ).matches() ) ) {
             logger.info( "Password is not correct." );
             errors.rejectValue( "password", "userValidator.password.invalid" );
         }
-    }
 
+        if ( !( user.getNewPassword().equals( user.getConfirmPassword() ) ) ) {
+            logger.info( "Password and PasswordControl is not the same." );
+            errors.rejectValue( "passwordControl", "userValidator.passwordControl.invalid" );
+        }
+    }
 }
