ec257c9131c703974a52e3bf487510001377b536
LoicPi <lcpiriou@gmail.com>
2019-08-27 15:24:18 +0200

    Modification de la classe MailConfig Changement des paramètres pour le mail d'envoi des mails d'informations.

--- src/main/java/com/adle/projet/config/MailConfig.java
+++ src/main/java/com/adle/projet/config/MailConfig.java
@@ -16,8 +16,8 @@ public class MailConfig {
         mailSender.setHost( "smtp.gmail.com" );
         mailSender.setPort( 587 );
 
-        mailSender.setUsername( "lesamisdelescalade@gmail.com" );
-        mailSender.setPassword( "lesadles" );
+        mailSender.setUsername( "lcpiriou@gmail.com" );
+        mailSender.setPassword( "alexia*1" );
 
         Properties props = mailSender.getJavaMailProperties();
         props.put( "mail.transport.protocol", "smtp" );
