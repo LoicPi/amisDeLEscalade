5d6b8cfb9980fbbf9833a84bd55d3a9ad90f4a11
LoicPi <lcpiriou@gmail.com>
2019-08-28 11:46:20 +0200

    Création de la classe Spot correspondant au site d'escalade. Lien de fait avec la table user pour associé un utilisateur à un site.

---
+++ src/main/java/com/adle/projet/entity/Spot.java
@@ -0,0 +1,142 @@
+package com.adle.projet.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table( name = "spots" )
+
+public class Spot {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.AUTO )
+    @Column( name = "id" )
+    private Integer id;
+
+    @Column( name = "spot_name", unique = true )
+    @Size( max = 100, min = 3, message = "{site.name.invalid}" )
+    private String  spotName;
+
+    @Column( name = "spot_county" )
+    private Integer spotCounty;
+
+    @Column( name = "spot_city" )
+    private String  spotCity;
+
+    @Column( name = "spot_country" )
+    private String  spotCountry;
+
+    @Column( name = "spot_descriptive" )
+    @Size( max = 600, min = 10, message = "{spot.descriptive.invalid}" )
+    private String  spotDescriptive;
+
+    @Column( name = "spot_access" )
+    @Size( max = 300, min = 10, message = "{spot.access.invalid}" )
+    private String  spotAccess;
+
+    @Column( name = "spot_type" )
+    @Size( max = 8, min = 3, message = "{spot.type.invalid" )
+    private String  spotType;
+
+    @Column( name = "spot_tag" )
+    private Boolean spotTag = false;
+
+    @ManyToOne( fetch = FetchType.LAZY )
+    @JoinColumn( name = "user_id" )
+    private User    userId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId( Integer id ) {
+        this.id = id;
+    }
+
+    public String getSpotName() {
+        return spotName;
+    }
+
+    public void setSpotName( String spotName ) {
+        this.spotName = spotName;
+    }
+
+    public Integer getSpotCounty() {
+        return spotCounty;
+    }
+
+    public void setSpotCounty( Integer spotCounty ) {
+        this.spotCounty = spotCounty;
+    }
+
+    public String getSpotCity() {
+        return spotCity;
+    }
+
+    public void setSpotCity( String spotCity ) {
+        this.spotCity = spotCity;
+    }
+
+    public String getSpotCountry() {
+        return spotCountry;
+    }
+
+    public void setSpotCountry( String spotCountry ) {
+        this.spotCountry = spotCountry;
+    }
+
+    public String getSpotDescriptive() {
+        return spotDescriptive;
+    }
+
+    public void setSpotDescriptive( String spotDescriptive ) {
+        this.spotDescriptive = spotDescriptive;
+    }
+
+    public String getSpotAccess() {
+        return spotAccess;
+    }
+
+    public void setSpotAccess( String spotAccess ) {
+        this.spotAccess = spotAccess;
+    }
+
+    public String getSpotType() {
+        return spotType;
+    }
+
+    public void setSpotType( String spotType ) {
+        this.spotType = spotType;
+    }
+
+    public Boolean getSpotTag() {
+        return spotTag;
+    }
+
+    public void setSpotTag( Boolean spotTag ) {
+        this.spotTag = spotTag;
+    }
+
+    public User getUserId() {
+        return userId;
+    }
+
+    public void setUserId( User userId ) {
+        this.userId = userId;
+    }
+
+    @Override
+    public String toString() {
+        return "Spot {id=" + id + ", spotName =" + spotName + ",spotCity =" + spotCity + ",spotCounty =" + spotCounty
+                + ",spotCountry =" + spotCountry + ", spotDescriptive =" + spotDescriptive +
+                ", spotAccess=" + spotAccess + ", spotType =" + spotType + ", userId =" + userId + "}";
+    }
+}
