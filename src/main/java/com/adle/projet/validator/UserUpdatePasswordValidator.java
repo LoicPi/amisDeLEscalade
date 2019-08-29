edb70be92f92245e191df19d9b2ceae06f424b2a
LoicPi <lcpiriou@gmail.com>
2019-08-29 14:31:32 +0200

    Modification sur la classe UserUpdatePassword Remplacement de UpdateUser par UpdatePasswordUser. Remplacement dans la methode errors.rejectValue des bon liens de la jsp. Ajout d'une validation sur l'ancien mdp afin de vérifier qu'il correspond à celui de la bdd.

--- src/main/java/com/adle/projet/validator/UserUpdatePasswordValidator.java
+++ src/main/java/com/adle/projet/validator/UserUpdatePasswordValidator.java
@@ -4,52 +4,66 @@ import java.util.regex.Pattern;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
-import com.adle.projet.dto.UpdateUser;
+import com.adle.projet.dto.UpdatePasswordUser;
+import com.adle.projet.entity.User;
+import com.adle.projet.service.UserService;
+import com.adle.projet.tools.PasswordEncryptor;
 
 @Component
 public class UserUpdatePasswordValidator implements Validator {
 
     private static final Logger logger = LogManager.getLogger( UserValidator.class );
 
+    @Autowired
+    private UserService         userService;
+
     @Override
     public boolean supports( Class<?> clazz ) {
-        return UpdateUser.class.equals( clazz );
+        return UpdatePasswordUser.class.equals( clazz );
     }
 
     @Override
     public void validate( Object obj, Errors errors ) {
-        UpdateUser user = (UpdateUser) obj;
+        UpdatePasswordUser user = (UpdatePasswordUser) obj;
+
+        User userPasswordCheck = userService.getUser( user.getId() );
 
         Pattern patternPassword = Pattern.compile(
                 "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[-+!*$@%_])([-+!*$@%_\\w]{8,15})$", Pattern.CASE_INSENSITIVE );
 
         if ( user.getOldPassword().equals( "" ) ) {
             logger.info( "Password is empty." );
-            errors.rejectValue( "password", "userLoggValidator.password.empty" );
+            errors.rejectValue( "oldPassword", "userLoggValidator.password.empty" );
         }
 
         if ( user.getNewPassword().equals( "" ) ) {
             logger.info( "Password is empty." );
-            errors.rejectValue( "password", "userLoggValidator.password.empty" );
+            errors.rejectValue( "newPassword", "userLoggValidator.password.empty" );
         }
 
         if ( user.getConfirmPassword().equals( "" ) ) {
             logger.info( "Password is empty." );
-            errors.rejectValue( "password", "userLoggValidator.password.empty" );
+            errors.rejectValue( "confirmPassword", "userLoggValidator.password.empty" );
         }
 
         if ( !user.getNewPassword().equals( "" ) && !( patternPassword.matcher( user.getNewPassword() ).matches() ) ) {
             logger.info( "Password is not correct." );
-            errors.rejectValue( "password", "userValidator.password.invalid" );
+            errors.rejectValue( "newPassword", "userValidator.password.invalid" );
         }
 
         if ( !( user.getNewPassword().equals( user.getConfirmPassword() ) ) ) {
             logger.info( "Password and PasswordControl is not the same." );
-            errors.rejectValue( "passwordControl", "userValidator.passwordControl.invalid" );
+            errors.rejectValue( "confirmPassword", "userValidator.passwordControl.invalid" );
+        }
+
+        if ( !( PasswordEncryptor.checkPassword( user.getOldPassword(), userPasswordCheck.getPassword() ) ) ) {
+            logger.info( "The old password is not correct." );
+            errors.rejectValue( "oldPassword", "userUpdateValidator.oldPassword.invalid" );
         }
     }
 }
