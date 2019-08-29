cc7a4d5801cc2389c6e58ee4943e05de8a652638
LoicPi <lcpiriou@gmail.com>
2019-08-29 12:24:03 +0200

    Création de la classe UpdateUser Création d'un dto pour effectuer la mise à jour d'un utilisateur.

--- src/main/java/com/adle/projet/dto/UpdateUser.java
+++ src/main/java/com/adle/projet/dto/UpdateUser.java
@@ -1,35 +1,81 @@
 package com.adle.projet.dto;
 
+import com.adle.projet.entity.Role;
+
 public class UpdateUser {
 
-    private String oldPassword;
+    private Integer id;
 
-    private String newPassword;
+    private String  firstName;
 
-    private String confirmPassword;
+    private String  lastName;
 
-    public String getOldPassword() {
-        return oldPassword;
+    private String  nickName;
+
+    private String  email;
+
+    private Role    userRole;
+
+    private Boolean userMember;
+
+    public boolean isMember() {
+        return userRole.getRoleCode().equals( "member" );
     }
 
-    public void setOldPassword( String oldPassword ) {
-        this.oldPassword = oldPassword;
+    public Integer getId() {
+        return id;
     }
 
-    public String getNewPassword() {
-        return newPassword;
+    public void setId( Integer id ) {
+        this.id = id;
     }
 
-    public void setNewPassword( String newPassword ) {
-        this.newPassword = newPassword;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public String getConfirmPassword() {
-        return confirmPassword;
+    public void setFirstName( String firstName ) {
+        this.firstName = firstName;
     }
 
-    public void setConfirmPassword( String confirmPassword ) {
-        this.confirmPassword = confirmPassword;
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName( String lastName ) {
+        this.lastName = lastName;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName( String nickName ) {
+        this.nickName = nickName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail( String email ) {
+        this.email = email;
+    }
+
+    public Role getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole( Role userRole ) {
+        this.userRole = userRole;
+    }
+
+    public Boolean getUserMember() {
+        return userMember;
+    }
+
+    public void setUserMember( Boolean userMember ) {
+        this.userMember = userMember;
     }
 
 }
