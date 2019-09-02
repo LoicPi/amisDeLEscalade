3cf9eb070dea51232e0a3025eb69568179e3ed6b
LoicPi <lcpiriou@gmail.com>
2019-09-02 12:56:58 +0200

    Création des classes interface SpotDAO et de son impléments SpotDAOImpl. Création des différentes méthodes pour obtenir la liste, faire la sauvegarde, l'obtention, la maj, l'obtention par utiliseur et l'effacement d'un site.

---
+++ src/main/java/com/adle/projet/dao/SpotDAO.java
@@ -0,0 +1,21 @@
+package com.adle.projet.dao;
+
+import java.util.List;
+
+import com.adle.projet.entity.Spot;
+
+public interface SpotDAO {
+
+    public List<Spot> getSpots();
+
+    public void saveSpot( Spot spot );
+
+    public Spot getSpot( int theId );
+
+    public void updateSpot( Spot spot );
+
+    public List<Spot> findSpotByUserId( int userId );
+
+    public void deleteSpot( int theId );
+
+}
---
+++ src/main/java/com/adle/projet/dao/SpotDAOImpl.java
@@ -0,0 +1,79 @@
+package com.adle.projet.dao;
+
+import java.util.List;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.adle.projet.entity.Spot;
+
+public class SpotDAOImpl implements SpotDAO {
+
+    private static final Logger logger = LogManager.getLogger( SpotDAOImpl.class );
+
+    @Autowired
+    private SessionFactory      sessionFactory;
+
+    @Override
+    public List<Spot> getSpots() {
+        Session session = sessionFactory.getCurrentSession();
+        CriteriaBuilder cb = session.getCriteriaBuilder();
+        CriteriaQuery<Spot> cq = cb.createQuery( Spot.class );
+        Root<Spot> root = cq.from( Spot.class );
+        cq.select( root );
+        Query query = session.createQuery( cq );
+        logger.info( "Spot List : " + query.getResultList() );
+        return query.getResultList();
+    }
+
+    @Override
+    public void saveSpot( Spot spot ) {
+        Session currentSession = sessionFactory.getCurrentSession();
+        currentSession.saveOrUpdate( spot );
+        logger.info( "Spot saved successfully, Spot details = " + spot );
+
+    }
+
+    @Override
+    public Spot getSpot( int theId ) {
+        Session currentSession = sessionFactory.getCurrentSession();
+        Spot spot = currentSession.get( Spot.class, theId );
+        logger.info( "Spot loaded successfully, Topo details = " + spot );
+        return spot;
+    }
+
+    @Override
+    public void updateSpot( Spot spot ) {
+        Session currentsession = sessionFactory.getCurrentSession();
+        currentsession.update( spot );
+        logger.info( "Spot updated successfully, Spot details = " + spot );
+
+    }
+
+    @Override
+    public List<Spot> findSpotByUserId( int userId ) {
+        Session currentSession = sessionFactory.getCurrentSession();
+        Query<Spot> query = currentSession.createNamedQuery( "Spot_findByUserId", Spot.class );
+        query.setParameter( "userId", userId );
+        List<Spot> spotResult = query.getResultList();
+        logger.info( "Spot List : " + query.getResultList() );
+        return spotResult;
+    }
+
+    @Override
+    public void deleteSpot( int theId ) {
+        Session session = sessionFactory.getCurrentSession();
+        Spot theSpot = session.byId( Spot.class ).load( theId );
+        session.delete( theSpot );
+
+    }
+
+}
