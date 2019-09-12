dbbb06b914474a46e78fce506da71145ed92f63a
LoicPi <lcpiriou@gmail.com>
2019-09-12 15:46:29 +0200

    Création des classes de validation pour l'objet Length et le DTO UpdateLength.

---
+++ src/main/java/com/adle/projet/validator/LengthUpdateValidator.java
@@ -0,0 +1,48 @@
+package com.adle.projet.validator;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import com.adle.projet.dto.UpdateLength;
+
+@Component
+public class LengthUpdateValidator implements Validator {
+
+    private static final Logger logger = LogManager.getLogger( LengthUpdateValidator.class );
+
+    @Override
+    public boolean supports( Class<?> clazz ) {
+        return UpdateLength.class.equals( clazz );
+    }
+
+    @Override
+    public void validate( Object obj, Errors errors ) {
+
+        UpdateLength updateLength = (UpdateLength) obj;
+
+        if ( updateLength.getUpdateLengthRelay() < 0 ) {
+            logger.info( "UpdateLength_Relay can't be negative" );
+            errors.rejectValue( "updateLengthRelay", "lengthValidator.lengthRelay.negative" );
+        }
+
+        if ( ( updateLength.getUpdateLengthRelay() == 0 ) && ( updateLength.getUpdateLengthSpit() == true ) ) {
+            logger.info( "UpdateLength_Relay can't be null" );
+            errors.rejectValue( "lengthRelay", "lengthValidator.lengthRelay.empty" );
+        }
+
+        if ( ( updateLength.getUpdateLengthRelay() > 0 ) && ( updateLength.getUpdateLengthSpit() == false ) ) {
+            logger.info( "UpdateLength_Spit can't be false" );
+            errors.rejectValue( "lengthSpit", "lengthValidator.lengthSpit.false" );
+        }
+
+        if ( updateLength.getUpdateLengthHeight() < 0 ) {
+            logger.info( "UpdateLength_Heigth can't be negative" );
+            errors.rejectValue( "updateLengthHeigth", "lengthValidator.lengthHeigth.negative" );
+        }
+
+    }
+
+}
---
+++ src/main/java/com/adle/projet/validator/LengthValidator.java
@@ -0,0 +1,47 @@
+package com.adle.projet.validator;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import com.adle.projet.entity.Length;
+
+@Component
+public class LengthValidator implements Validator {
+
+    private static final Logger logger = LogManager.getLogger( LengthValidator.class );
+
+    @Override
+    public boolean supports( Class<?> clazz ) {
+        return Length.class.equals( clazz );
+    }
+
+    @Override
+    public void validate( Object obj, Errors errors ) {
+
+        Length length = (Length) obj;
+
+        if ( length.getLengthRelay() < 0 ) {
+            logger.info( "Length_Relay can't be negative" );
+            errors.rejectValue( "lengthRelay", "lengthValidator.lengthRelay.negative" );
+        }
+
+        if ( ( length.getLengthRelay() == 0 ) && ( length.getLengthSpit() == true ) ) {
+            logger.info( "Length_Relay can't be null" );
+            errors.rejectValue( "lengthRelay", "lengthValidator.lengthRelay.empty" );
+        }
+
+        if ( ( length.getLengthRelay() > 0 ) && ( length.getLengthSpit() == false ) ) {
+            logger.info( "Length_Spit can't be false" );
+            errors.rejectValue( "lengthSpit", "lengthValidator.lengthSpit.false" );
+        }
+
+        if ( length.getLengthHeight() < 0 ) {
+            logger.info( "Length_Heigth can't be negative" );
+            errors.rejectValue( "lengthHeigth", "lengthValidator.lengthHeigth.negative" );
+        }
+    }
+
+}
