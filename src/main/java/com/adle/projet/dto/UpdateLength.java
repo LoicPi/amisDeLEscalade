3f6429766607ccd14dcc1a4f6ae68a4e2e49d08b
LoicPi <lcpiriou@gmail.com>
2019-09-12 15:45:27 +0200

    Création d'un DTO de maj pour l'objet Length

---
+++ src/main/java/com/adle/projet/dto/UpdateLength.java
@@ -0,0 +1,65 @@
+package com.adle.projet.dto;
+
+public class UpdateLength {
+
+    private Integer id;
+
+    private Integer updateLengthHeight;
+
+    private Integer updateLengthRelay;
+
+    private Boolean updateLengthSpit;
+
+    private String  updateLengthLevel;
+
+    private String  updateLengthListing;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId( Integer id ) {
+        this.id = id;
+    }
+
+    public Integer getUpdateLengthHeight() {
+        return updateLengthHeight;
+    }
+
+    public void setUpdateLengthHeight( Integer updateLengthHeight ) {
+        this.updateLengthHeight = updateLengthHeight;
+    }
+
+    public Integer getUpdateLengthRelay() {
+        return updateLengthRelay;
+    }
+
+    public void setUpdateLengthRelay( Integer updateLengthRelay ) {
+        this.updateLengthRelay = updateLengthRelay;
+    }
+
+    public Boolean getUpdateLengthSpit() {
+        return updateLengthSpit;
+    }
+
+    public void setUpdateLengthSpit( Boolean updateLengthSpit ) {
+        this.updateLengthSpit = updateLengthSpit;
+    }
+
+    public String getUpdateLengthLevel() {
+        return updateLengthLevel;
+    }
+
+    public void setUpdateLengthLevel( String updateLengthLevel ) {
+        this.updateLengthLevel = updateLengthLevel;
+    }
+
+    public String getUpdateLengthListing() {
+        return updateLengthListing;
+    }
+
+    public void setUpdateLengthListing( String updateLengthListing ) {
+        this.updateLengthListing = updateLengthListing;
+    }
+
+}
